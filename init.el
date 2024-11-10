b53c4490c2add3f36cc1cd7588a0f2717e8dbe17
Jalen Moore <jalenmoore@Jalens-MacBook-Pro.local>
2024-11-10 00:57:08 -0600

    A bare bones emacs.

--- init.el
+++ init.el
@@ -1,45 +1,114 @@
-;;
-;;                 _    
-;;    __   ___.--'_`.   
-;;   ( _`.'. -   'o` )    A quite awful emacs config
-;;   _\.'_'      _.-'        by: Jalen Moore
-;;  ( \`. )    //\`          art by: Veronica Karlsson (Found at: www.asciiart.eu)
-;;   \_`-'`---'\\__,    
-;;    \`        `-\     
-;;     `                  
-;;
+;; ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡖⠀⡔⠀⠀⠀⠀⠀⠰⡰⡀⠀⠀⢳⣄⠀⠀⠐⠆⠀⠀⠀⠀⠀⠐⢆⠀⠀⠀⠀⠀
+;; ⠀⠀⠀⠀⠀⢀⡔⠛⠛⢯⡉⠑⠒⡤⠊⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⣀⣀⡀⠤⠤⠤⠤⠤⠼⠤⢴⣃⡀⠀⠀⠀⠀⠀⢳⢱⡀⠀⠀⢳⡱⣄⠀⠀⠐⢄⠀⠀⠀⠀⠈⢣⠀⠀⠀⠀
+;; ⠀⠀⠀⠀⢠⠋⠀⠀⣀⣀⢳⣠⠎⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠒⠦⣄⠀⠈⡇⢣⠀⠀⠀⢣⠈⢦⠀⠀⠀⠑⢄⠀⠀⠀⠀⢣⡀⠀⠀
+;;  ⠀⠀⠀⢠⣃⠴⠊⠉⢀⡴⡻⢹⠘⠢⢄⣀⣀⠴⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠢⣱⠈⡇⠀⠀⠈⣇⠀⢷⡀⠀⠀⠈⠳⡀⠀⠀⠀⢳⡀⠀
+;; ⠀⠀⢀⠏⠁⠀⠀⢠⠎⡰⠁⠘⡄⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣽⠀⠀⠀⢸⠀⢸⡵⡀⠀⠀⠀⠘⢆⠀⠀⠀⢣⠀
+;; ⠀⠀⡜⠀⠀⠀⢠⠏⢰⡇⠀⠀⢑⠜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠄⠒⠒⠒⠒⠂⠤⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠀⠈⡆⢸⢇⢱⡀⠀⠀⠀⠈⢦⠀⠀⠈⠀
+;; ⠀⢠⠃⠀⠀⠀⡞⠀⣿⠁⠀⢠⠎⠀⠀⠀⠀⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠄⠀⠀⠤⠤⣀⡈⠑⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢄⠀⡇⢸⢸⠀⢧⠀⠀⠀⠀⠈⢧⣀⠴⠊
+;; ⠀⠈⠀⠀⠀⢸⠁⢸⢸⠀⣰⠃⠀⠀⠀⠀⠀⠀⡴⠋⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠢⢄⠑⢄⠀⠀⠀⢦⠀⠀⠀⠀⠀⠀⠈⢦⡇⢸⠘⡄⠘⡆⠀⠀⣀⠴⠊⠁⢀⡰
+;; ⠀⠀⠀⠀⠀⡇⠀⡎⢸⢠⠃⠀⠀⠀⠀⠀⢀⡞⠀⢠⠖⠁⠀⢤⡀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠢⣵⣄⠀⠀⠳⡀⠀⠀⠀⠀⠀⠀⢳⡌⠀⡇⠀⢷⡠⠊⠁⠀⣠⡔⡏⡜
+;;  ⠀⠀⠀⠀⢸⠀⠀⡇⢸⡎⠀⠀⠀⠀⠀⢀⡎⠀⡴⠁⠀⠀⠀⠀⢹⢦⡈⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⣄⠀⠹⡄⠀⠀⠀⠀⠀⠀⢻⣠⣃⡴⠋⠀⣠⠔⠊⠀⡇⢩⠞
+;;  ⠀⠀⠀⠀⡏⠀⠀⡇⡸⠀⠀⠀⠀⠀⠀⡞⠀⡜⠁⠀⠀⠀⠀⠀⢸⡆⠛⠦⡈⠢⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⠀⠘⡄⠀⠀⠀⠀⠀⠀⢻⡊⠀⣠⠊⠁⠀⠀⡜⢰⡇⠀
+;;  ⠀⠀⠀⢠⠃⠀⠀⣇⠇⠀⠀⠀⠀⠀⢰⠁⡸⠁⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠈⠓⠬⡑⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠹⡀⠀⠀⠀⠀⢠⡀⢣⡜⠁⠀⠀⠀⢰⢱⠃⢸⠀
+;;  ⠀⠀⠀⢸⠀⠀⠀⢹⢠⠀⠀⠀⠀⠀⢸⢠⠃⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠈⠑⠲⠭⣲⡴⢖⣒⡆⠀⠀⠀⠀⠀⢧⠀⢳⠀⠀⠀⠀⠀⢧⠈⡆⠀⠀⠀⢠⢇⠇⠀⠸⠀
+;;  ⠀⠀⠀⡎⠀⠀⠀⡎⢸⠀⠀⠀⠀⠀⢿⡜⠀⠀⠀⠀⠀⠀⡀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⢉⡡⠞⠛⢭⣭⣥⠤⠤⠚⡆⠈⣦⠆⠀⠀⠀⠸⠀⢳⠀⠀⠀⡞⡞⠀⠀⠀⠀
+;;  ⠀⠀⠀⡇⠀⠀⢰⡇⢸⠀⠀⠀⠀⠀⢸⠇⠀⠀⠀⠀⠀⢠⡏⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⠉⠉⣠⠖⠋⠁⢀⣤⣤⣦⣔⣳⡾⢳⠀⠀⠀⠀⠀⡇⢸⣀⡠⠊⣼⢸⠀⠀⠀⠀
+;;  ⠀⠀⠀⡇⠀⠀⢸⡇⢸⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⠀⢀⡮⣵⠓⢠⡤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡝⢀⣴⣯⣿⡿⣛⣿⣿⣻⡄⢸⠀⠀⠀⠀⢀⡇⠸⠣⣄⡜⠁⢸⠀⠀⠀⠀
+;;  ⠀⠀⠀⡇⠀⠀⡇⠇⢸⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⢴⣱⣷⣁⢤⣊⣀⡀⠙⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⡿⠁⢿⣄⡿⠟⣸⡛⡇⡎⠀⠀⠀⠀⢸⣵⢐⣴⠋⠀⠀⠈⠀⠀⠀⠀
+;;  ⠯⣉⠀⠓⠒⠒⠃⢼⠸⡄⠀⠀⠀⠀⠀⠸⡀⠀⢀⣼⠏⡠⠞⠁⠀⠈⠙⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⠀⠐⣲⡾⠏⠀⣧⢣⠀⠀⠀⠀⡎⣿⣼⢸⡆⠀⠀⠀⠀⠀⠀⠀
+;;  ⠣⢌⡻⣍⣱⠒⡠⢼⠀⡇⠀⠀⠀⠀⠀⠀⢧⡠⠚⠁⠘⢁⣤⣶⣶⣶⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡟⢸⠀⠀⠀⢰⢹⡜⣿⣿⢣⠀⠀⠀⠀⠀⠀⠀
+;;  ⠀⠀⠉⢲⣄⡀⢣⡈⡇⢹⠀⠀⠀⠀⠀⢢⠘⡄⢄⣀⣠⣾⠟⢹⣏⡈⣿⡿⣿⠲⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠀⢸⠀⠀⢠⢃⡏⡇⡿⣹⢸⡀⠀⠀⠀⠀⠀⠀
+;;  ⠀⠀⠀⢸⣡⠉⡶⣍⢳⠀⣇⠀⠀⠀⠀⠈⣆⠻⣄⠀⠉⠙⢦⣀⠉⠹⠧⣤⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠁⠀⢸⠀⣠⠃⡞⡇⡇⠀⡟⡞⠁⠀⠀⠀⠀⢀⠀
+;;  ⠀⠀⠀⠀⡿⠀⡇⠀⠹⡄⢸⡄⠀⠀⠀⠀⠘⡄⢣⠳⣄⠀⠀⠀⠋⠚⠙⠉⠁⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⢸⡰⠁⡜⠁⡗⢸⠀⢹⠃⠀⠃⠀⠀⠀⠸⠀
+;;  ⠀⠀⠀⠀⢧⡇⠃⠀⠀⢳⠈⡿⡀⠀⠀⠀⠀⠘⣆⢳⡌⣢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢯⣼⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠜⢁⡞⠀⢰⠃⣼⠀⢸⠀⠀⠀⠀⠀⠀⡇⠀
+;;  ⠀⠀⠀⠀⢸⢡⠀⠀⠀⠈⢧⢱⢱⡀⠀⠀⠀⠀⢹⢫⣫⡁⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⠀⠀⠀⠀⠀⢀⡾⠁⠀⡜⢠⢻⠀⢸⠀⠠⠀⠀⠀⢠⠃⠀
+;;  ⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠈⣎⢿⠙⣄⠀⠀⠀⠈⣿⢯⠳⣄⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠐⠊⠁⠀⠀⠀⠀⠀⠀⡰⠋⠀⠀⣼⢥⠏⡸⠀⡎⡄⠀⠀⠀⠀⡸⠀⠀
+;;  ⠀⠀⠀⠀⠀⢳⠀⠀⠀⠀⠀⢸⢞⣞⡞⢳⡀⠸⡄⠛⡌⣦⠏⠷⠄⠀⠑⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠀⠀⣠⡾⡿⠋⢀⡇⢀⢧⠃⠀⠀⠀⠀⡇⠀⠀
+;;   ⠀⠀⠀⠀⠸⡄⠀⠀⠀⠀⢸⢸⣿⣦⣞⠏⠢⣱⢀⢧⢹⠀⠀⠀⠀⠀⠀⠈⠉⠒⠒⠠⠤⠤⢀⣀⣀⣀⠀⠀⠀⢀⡠⠞⠁⢀⣠⣾⣿⢾⡀⠀⠘⠀⢸⡜⠀⠀⠀⠀⠘⠀⡸⠀
+;;  ⠀⠀⠀⠀⠀⠀⢇⠀⠀⠀⠀⢸⢸⠿⡫⠋⠀⠀⠈⡟⠈⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⢿⣿⠯⠥⠤⠒⣎⡿⢿⡿⠳⡀⠑⣄⠀⠀⣏⠃⠀⠀⠀⠀⠀⢠⠃⠀
+;;  ⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⠀⢸⡈⠉⠀⠀⠀⠀⠀⠇⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢹⠍⢹⠉⢹⠀⠀⢀⣳⡄⠙⢄⠈⢢⣰⡻⠀⠀⠀⠀⠀⠀⡞⠀⠀
+;;  ⠀⠀⠀⠀⠀⠀⠘⣇⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⢸⠀⣸⠎⠉⠓⠢⢜⣆⠈⢣⡀⢹⠇⠀⠀⠀⠀⠀⢠⠃⠀⠀
+;;  ⠀⠀⠀⠀⠀⠀⠀⡿⡄⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠈⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡃⢸⡴⢻⠀⣀⣠⣤⣶⣾⣧⡀⠑⡼⠀⠀⠀⠀⠀⠀⡸⠀⠀⠀
+;;  ⠀⠀⡆⠀⠀⠀⠀⢸⢣⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠟⡇⢸⣟⢿⡿⠿⠿⠿⠛⣷⣄⡇⠀⠀⠀⠀⠀⠀⡇⠀⡴⠀
+;;  ⠀⠀⢱⠀⠀⠀⠀⠈⡎⡆⠀⠀⡷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⡇⢸⡇⠀⠀⣀⡠⠤⠖⠛⠚⡇⠀⢠⠀⠀⠀⢠⠃⢰⠃⠀
+;; 
+;; 			Comfy Emacs configuration ＜(´⌯ ̫⌯`)＞
+;; 			      For degenerates only...
+;; 
+;;  ascii: https://emojicombos.com/frieren%3A-beyond-journey's-end
 
-(setenv "PATH" (concat (getenv "PATH") ":/usr/bin/"))
-(setq exec-path (append exec-path '("/usr/bin")))
+;; -*- coding: utf-8; lexical-binding: t -*-
+(load-theme 'modus-vivendi)
+(setq inhibit-startup-screen t
+      use-package-always-ensure t
+      initial-scratch-message nil
+      sentence-end-double-space nil
+      use-dialog-box nil
+      mark-even-if-inactive nil
+      default-directory "~/"
+      display-line-numbers-mode 1
+      load-prefer-newer t)
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(delete-selection-mode t)
 
-(straight-use-package 'use-package)
-(setq package-enable-at-startup nil)
-(setq straight-use-package-by-default t)
+(add-to-list 'default-frame-alist '(alpha . 95))
 
-(require 'package)
+(set-charset-priority 'unicode)
+(prefer-coding-system 'utf-8-unix)
+
+; Auto save on timer.
+(defvar *auto-save-timer* nil)
+
+(defun mjalen/enable-auto-save ()
+  (interactive)
+  (setq *auto-save-timer* (run-with-idle-timer 3 t #'save-buffer)))
+
+(defun mjalen/disable-auto-save ()
+  (interactive)
+  (if *auto-save-timer* 
+	(cancel-timer *auto-save-timer*) 
+	(setq *auto-save-timer* nil)))
+
+;; Define modes to auto-save. It is safer to opt-in than to opt-out.
+(defconst *auto-save-hooks* '(emacs-lisp-mode-hook))
+(dolist (hook *auto-save-hooks*)
+  (add-hook hook #'mjalen/enable-auto-save))
+(add-hook 'change-major-mode-hook #'mjalen/disable-auto-save)
+ 
+(add-to-list 'default-frame-alist '(font . "Victor Mono-13"))
+(set-frame-font "Victor Mono-13")
+
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
-; Load emacs.org
-(use-package org)
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+; Define packages here.
+(custom-set-variables
+ '(package-selected-packages
+   '(package
+     recentf
+     icomplete
+     no-littering
+     evil)))
+
+; Make sure all packages are installed.
+(dolist (pkg package-selected-packages)
+  (require pkg))
+
+(with-eval-after-load 'icomplete
+  (fido-mode -1)
+  (icomplete-mode 1))
+
+(with-eval-after-load 'no-littering
+  (add-to-list 'recentf-exclude no-littering-var-directory)
+  (add-to-list 'recentf-exclude no-littering-etc-directory)
+  (minibuffer-message "Loaded no-littering"))
+
+(with-eval-after-load 'evil
+  (evil-mode 1))
+
+(custom-set-faces)
